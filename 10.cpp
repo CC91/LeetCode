f9f7cc21444f4485d78b16c5b91086ea00f3c3c2
CC <caocancabbage@gmail.com>
2016-01-26 15:18:15 -0600

    10

---
+++ 10.cpp
@@ -0,0 +1,25 @@
+// 10. Regular Expression Matching
+
+class Solution {
+public:
+    bool isMatch(string s, string p) {
+        int m = s.size(), n = p.size();
+        vector<vector<bool>> dp(m+1, vector<bool> (n+1, false));
+        dp[0][0] = true;
+        for (int i=0; i<=m; i++) {
+            for (int j=0; j<=n; j++) {
+                if (p[j-1]!='.' && p[j-1]!='*') {
+                    if (i>0 && s[i-1]==p[j-1] && dp[i-1][j-1]) dp[i][j] = true;
+                }
+                else if (p[j-1]=='.') {
+                    if (i>0 && dp[i-1][j-1]) dp[i][j] = true;
+                }
+                else if (j>1) {
+                    if (dp[i][j-1] ||dp[i][j-2]) dp[i][j] = true; // match 1 or 0 preceding elements
+                    else if (i>0 && (p[j-2]==s[i-1] ||p[j-2]=='.') && dp[i-1][j]) dp[i][j] = true;
+                }
+            }
+        }
+        return dp[m][n];
+    }
+};
