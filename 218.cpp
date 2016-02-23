b057e8022aa9e920747b64abbb6afefb24b98d08
CC <caocancabbage@gmail.com>
2016-02-23 09:34:50 -0600

    218

--- 218.cpp
+++ 218.cpp
@@ -1,22 +1,22 @@
 // 218. The Skyline Problem
 
 class Solution {
-public:
+public: // http://www.cnblogs.com/grandyang/p/4534586.html
     vector<pair<int, int>> getSkyline(vector<vector<int>>& buildings) {
         vector<pair<int, int>> boundary, res;
         multiset<int> h;
-        int pre = 0, cur = 0;
+        h.insert(0);
+        int cur = 0, pre = 0;
         for (auto a:buildings) {
             boundary.push_back({a[0], -a[2]});
             boundary.push_back({a[1], a[2]});
         }
         sort(boundary.begin(), boundary.end());
-        h.insert(0);
         for (auto a:boundary) {
             if (a.second<0) h.insert(-a.second);
             else h.erase(h.find(a.second));
             cur = *h.rbegin();
-            if (cur!=pre) {
+            if (pre!=cur) {
                 res.push_back({a.first, cur});
                 pre = cur;
             }
