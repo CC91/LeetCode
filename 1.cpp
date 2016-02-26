fdb259f3577fdef4c57f02b6878f9fc571b1116e
CC <caocancabbage@gmail.com>
2016-02-26 16:39:13 -0600

    1

--- 1.cpp
+++ 1.cpp
@@ -1,38 +1,33 @@
 // 1. Two Sum
 
 class Solution {
-public:// brute force
+public: // hash table
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> res;
+        unordered_map<int, int> m;
+        for (int i=0; i<nums.size(); i++) {
+            if (m.count(target-nums[i])) {
+                res.push_back(m[target-nums[i]]);
+                res.push_back(i);
+                return res;
+            } else m[nums[i]]=i;
+        }
+    }
+};
+
+
+class Solution {
+public: // brute force
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> res;
         for (int i=0; i<nums.size()-1; i++) {
             for (int j=i+1; j<nums.size(); j++) {
                 if (nums[i]+nums[j]==target) {
-                    res.push_back(i+1);
-                    res.push_back(j+1);
+                    res.push_back(i);
+                    res.push_back(j);
+                    return res;
                 }
             }
         }
-        return res;
-    }
-};
-
-class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> res;
-        vector<int> tmp = nums;
-        sort(tmp.begin(), tmp.end());
-        int a = 0, b = tmp.size()-1;
-        while (a<b) {
-            if (tmp[a]+tmp[b]<target) a++;
-            else if (tmp[a]+tmp[b]>target) b--;
-            else break;
-        }
-        
-        for (int i=0; i<nums.size(); i++) {
-            if (nums[i]==tmp[a] ||nums[i]==tmp[b]) res.push_back(i+1);
-        }
-        if (res[0]>res[1]) swap(res[0], res[1]);
-        return res;
     }
 };
