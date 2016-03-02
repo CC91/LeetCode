8e734c9dbcc24524821a7e7f1019c1cabcfd0c68
CC <caocancabbage@gmail.com>
2016-03-02 14:57:11 -0600

    42

--- 42.cpp
+++ 42.cpp
@@ -1,23 +1,22 @@
 // 42. Trapping Rain Water
 
 class Solution {
-public:
+public: // use a stack to track the decreasing and increasing sink with their indices
     int trap(vector<int>& height) {
-        int n = height.size();
-        if (n<3) return 0;
-        int res = 0;
+        int n = height.size(), res = 0;
+        if (n<3) return res;
         stack<int> s;
         s.push(0);
         for (int i=1; i<n; i++) {
             if (height[i]>height[s.top()]) {
-                int y = height[s.top()];
+                int bottom = height[s.top()];
                 s.pop();
                 while (!s.empty() && height[i]>=height[s.top()]) {
-                    res += (height[s.top()]-y)*(i-s.top()-1);
-                    y = height[s.top()];
+                    res += (height[s.top()]-bottom)*(i-s.top()-1);
+                    bottom = height[s.top()];
                     s.pop();
                 }
-                if (!s.empty()) res += (height[i]-y)*(i-s.top()-1);
+                if (!s.empty() && height[i]<height[s.top()]) res += (height[i]-bottom)*(i-s.top()-1); // corner case
             }
             s.push(i);
         }
@@ -27,18 +26,16 @@ public:
 
 
 class Solution {
-public:
+public: // 2 pointers represent the left height and right height of any position
     int trap(vector<int>& height) {
-        int n = height.size();
-        if (n<3) return 0;
-        vector<int> left(n, 0);
-        vector<int> right(n, 0);
-        int res = 0;
-        for (int i=1; i<n; i++) left[i] = max(left[i-1], height[i-1]);
-        for (int i=n-2; i>=0; i--) {
-            right[i] = max(right[i+1], height[i+1]);
-            int minH = min(left[i], right[i]);
-            if (minH>height[i]) res += (minH-height[i]);
+        int n = height.size(), res = 0;
+        if (n<3) return res;
+        vector<int> l(n, 0), r(n, 0);
+        for (int i=1; i<n; i++) l[i] = max(l[i-1], height[i-1]);
+        for (int i=n-2; i>=0; i--) { // right part must start from right
+            r[i] = max(r[i+1], height[i+1]);
+            int minHeight = min(l[i], r[i]);
+            if (minHeight>height[i]) res += minHeight-height[i];
         }
         return res;
     }
