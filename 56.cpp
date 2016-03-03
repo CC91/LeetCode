75bb9b01a7f2decb5c49c9c3d8b6921d9cd6e821
CC <caocancabbage@gmail.com>
2016-03-03 15:33:39 -0600

    56

--- 56.cpp
+++ 56.cpp
@@ -1,6 +1,6 @@
-// 56. Merge Intervals
+ // 56. Merge Intervals
 
-/**
+ /**
  * Definition for an interval.
  * struct Interval {
  *     int start;
@@ -10,19 +10,18 @@
  * };
  */
 class Solution {
-public:
+public: // define a comparison operator
     struct comp {
         bool operator() (Interval a, Interval b) {
             return a.start<b.start;
         }
     };
-    
     vector<Interval> merge(vector<Interval>& intervals) {
         vector<Interval> res;
-        sort(intervals.begin(), intervals.end(), comp());
+        sort(intervals.begin(), intervals.end(), comp()); // comparison
         for (int i=0; i<intervals.size(); i++) {
-            if (res.empty() ||res.back().end<intervals[i].start) res.push_back(intervals[i]); //no overlap
-            else res.back().end = max(res.back().end, intervals[i].end); // overlap
+            if (res.empty() ||res.back().end<intervals[i].start) res.push_back(intervals[i]);
+            else res.back().end = max(res.back().end, intervals[i].end);
         }
         return res;
     }
