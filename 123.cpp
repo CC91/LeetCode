91b904b5a7a25ddd7a92bf2dbc757597093a6260
CC <caocancabbage@gmail.com>
2016-03-09 11:34:05 -0600

    123

--- 123.cpp
+++ 123.cpp
@@ -1,23 +1,19 @@
 // 123. Best Time to Buy and Sell Stock III
 
 class Solution {
-public:
+public: // scan from left to right to get the first transaction max then from right to left to get the second max
     int maxProfit(vector<int>& prices) {
         if (prices.empty()) return 0;
         int n = prices.size();
-        vector<int> leftProfit(n, 0);
-        int minPrice = prices[0], maxLeftProfit = 0;
+        int res = 0, minPrice = prices[0], maxPrice = prices[n-1], maxRightProfit = 0;
+        vector<int>  leftProfit(n, 0);
         for (int i=1; i<n; i++) {
-            if (prices[i]<minPrice) {
-                minPrice = prices[i];
-            }
-            else maxLeftProfit = max(maxLeftProfit, prices[i]-minPrice);
-            leftProfit[i] = maxLeftProfit;
+            minPrice = min(prices[i], minPrice);
+            leftProfit[i] = max(prices[i]-minPrice, 0);
         }
-        int res = leftProfit[n-1], maxPrice = prices[n-1], maxRightProfit = 0;
         for (int i=n-2; i>=0; i--) {
-            if (prices[i]>maxPrice) maxPrice = prices[i];
-            else maxRightProfit = max(maxRightProfit, maxPrice-prices[i]);
+            maxPrice = max(prices[i], maxPrice);
+            maxRightProfit = max(maxPrice-prices[i], maxRightProfit);
             res = max(res, maxRightProfit+leftProfit[i]);
         }
         return res;
