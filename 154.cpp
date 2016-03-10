ae9a88af63488482b583dd727c6e175097f2ec95
CC <caocancabbage@gmail.com>
2016-03-10 11:08:27 -0600

    154

--- 154.cpp
+++ 154.cpp
@@ -4,10 +4,10 @@ class Solution {
 public:
     int findMin(vector<int>& nums) {
         int start = 0, end = nums.size()-1;
-        while (start<=end) {
+        while (start<end) {
             int mid = start+(end-start)/2;
-            if (nums[mid]<nums[end]) end=mid;
-            else if (nums[mid]>nums[end]) start=mid+1;
+            if (nums[mid]<nums[end]) end = mid; // right half is sorted
+            else if (nums[mid]>nums[end]) start = mid+1; // left half is sorted
             else end--;
         }
         return nums[start];
