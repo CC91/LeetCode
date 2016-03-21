963308269bf03f540c263f7625b461762bfc3445
CC <caocancabbage@gmail.com>
2016-03-21 08:49:17 -0500

    18

--- 18.cpp
+++ 18.cpp
@@ -30,3 +30,30 @@ public: // given 2 numbers, do 2sum to the rest of numbers
         return res;
     }
 };
+
+
+class Solution {
+public: // hash map, https://leetcode.com/discuss/3950/tle-on-4sum-using-hashtable
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
+        unordered_map<int, set<pair<int, int>>> m;
+        set<vector<int>> res;
+        int n = nums.size();
+        for (int i=0; i<n; i++) {
+            for (int j=i+1; j<n; j++) { // check map first
+                int a = nums[i]+nums[j];
+                if (m.count(target-a)) {
+                    for (auto p:m[target-a]) {
+                        vector<int> b = {p.first, p.second, nums[i], nums[j]};
+                        res.insert(b);
+                    }
+                }
+            }
+            for (int j=0; j<i; j++) { // then insert pair to the map in order to avoid duplicate elements in the solution
+                int a = nums[j], b = nums[i];
+                m[a+b].insert(make_pair(a, b));
+            }
+        }
+        return vector<vector<int>> (res.begin(), res.end());
+    }
+};
