247ae3fc307824af17ef316b4a3617f492594ae5
CC <caocancabbage@gmail.com>
2016-04-08 12:22:12 -0500

    23

--- 23.cpp
+++ 23.cpp
@@ -9,7 +9,7 @@
  * };
  */
 class Solution {
-public: // Divide and Conquer, merge every head and tail lists
+public: // D&Q, merge every 2 lists from both sides
     ListNode* mergeKLists(vector<ListNode*>& lists) {
         if (lists.empty()) return NULL;
         int end = lists.size()-1;
@@ -23,46 +23,45 @@ public: // Divide and Conquer, merge every head and tail lists
         }
         return lists[0];
     }
-    
     ListNode* merge2(ListNode* l1, ListNode* l2) {
-        ListNode* dummy = new ListNode(INT_MIN);
-        ListNode* tmp = dummy;
-        while (l1 && l2) {
+        ListNode* dummy = new ListNode(0);
+        ListNode* p = dummy;
+        while (l1&&l2) {
             if (l1->val<l2->val) {
-                tmp->next = l1;
+                p->next = l1;
                 l1 = l1->next;
             } else {
-                tmp->next = l2;
+                p->next = l2;
                 l2 = l2->next;
             }
-            tmp = tmp->next;
+            p = p->next;
         }
-        tmp->next = l1?l1:l2;
+        p->next = l1?l1:l2;
         return dummy->next;
     }
 };
 
 
 class Solution {
-public: // priority queue, since it is linked list, we can't use default greater/less operator in PQ. Define a new struct with bool operator
-    struct comp{
+public: // priority queue with bool operator
+    struct comp {
         bool operator() (ListNode* x, ListNode* y) {
             return x->val>y->val;
         }
     };
     
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        ListNode* dummy = new ListNode(INT_MIN);
-        ListNode* tmp = dummy;
+        ListNode* dummy = new ListNode(0);
+        ListNode* p = dummy;
         priority_queue<ListNode*, vector<ListNode*>, comp> q;
         for (int i=0; i<lists.size(); i++) {
             if (lists[i]) q.push(lists[i]);
         }
         while (!q.empty()) {
-            tmp->next = q.top();
+            p->next = q.top(); // smallest nodes are at the top
             q.pop();
-            tmp = tmp->next;
-            if (tmp->next) q.push(tmp->next);
+            p = p->next;
+            if (p->next) q.push(p->next);
         }
         return dummy->next;
     }
