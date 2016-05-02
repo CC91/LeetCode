6f9e0eafde04713c49a62cab9995fade177e06ed
CC <caocancabbage@gmail.com>
2016-05-02 16:17:42 -0500

    28

--- 28.cpp
+++ 28.cpp
@@ -3,16 +3,7 @@
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        if (needle.empty()) return 0;
-        int h = haystack.size(), n = needle.size(), i = 0;
-        while (i<h) {
-            if (haystack[i]==needle[0]) {
-                string tmp = haystack.substr(i, n);
-                if (tmp==needle) return i;
-            }
-            i++;
-        }
-        return -1;
+        return haystack.find(needle);
     }
 };
 
@@ -20,7 +11,33 @@ public:
 class Solution {
 public:
     int strStr(string haystack, string needle) {
-        int res = haystack.find(needle);
-        return res;
+        if (needle.empty()) return 0;
+        if (haystack.empty()) return -1;
+        int h = haystack.size(), n = needle.size();
+        for (int i=0; i<=h-n; i++) {
+            if (haystack[i]==needle[0]) {
+                string tmp = haystack.substr(i, n);
+                if (tmp==needle) return i;
+            }
+        }
+        return -1;
+    }
+};
+
+
+class Solution {
+public: // 2 pointers
+    int strStr(string haystack, string needle) {
+        int i = 0, j = 0;
+        while (haystack[i] && needle[j]) {
+            if (haystack[i]==needle[j]) {
+                i++;
+                j++;
+            } else {
+                i = i-j+1;
+                j = 0;
+            }
+        }
+        return needle[j]?-1:(i-j);
     }
 };
