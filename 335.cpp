b4805db6140d0f1e9aa7affd4a7b43bc27c63319
CC <caocancabbage@gmail.com>
2016-06-02 11:11:13 -0500

    325

--- 335.cpp
+++ 335.cpp
@@ -11,3 +11,23 @@ public:// https://www.hrwhisper.me/leetcode-self-crossing/
         return false;
     }
 };
+
+
+class Solution {
+public:
+    bool isSelfCrossing(vector<int>& x) {
+        x.insert(x.begin(), 4, 0);
+        int i = 4;
+        int len = x.size();
+        for (; i < len && x[i] > x[i - 2]; i++); // check outer spiral
+        if (i == len) {
+            return false;
+        }
+        
+        if (x[i - 4] + x[i] >= x[i - 2] ) { // check inner spiral
+            x[i - 1] -= x[i - 3];
+        }
+        for (i++; i < len && x[i] < x[i - 2]; i++);
+        return i != len;
+    }
+};
