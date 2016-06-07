fb5cf55f4bd444c37022ac1365c32221cd0d45ab
CC <caocancabbage@gmail.com>
2016-06-07 12:52:37 -0500

    306

--- 306.cpp
+++ 306.cpp
@@ -20,3 +20,36 @@ public: // |<---s1-->|<--s2-->|<--s3-->|, stoi(s1)+stoi(s2)=stoi(s3).
         return false;
     }
 };
+
+
+
+class Solution {
+public:
+    bool isAdditiveNumber(string num) {
+        for (int i = 1; i < num.size(); i++) {
+            for (int j = i + 1; j < num.size(); j++) {
+                string s1 = num.substr(0, i);
+                string s2 = num.substr(i, j - i);
+                if ((s1.size() > 1 && s1[0] == '0') || (s2.size() > 1 && s2[0] == '0')) {
+                    continue;
+                }
+                long long n1 = stoll(s1);
+                long long n2 = stoll(s2);
+                long long n3 = n1 + n2;
+                string s3 = to_string(n1 + n2);
+                string tmp = s1 + s2 + s3;
+                while (tmp.size() < num.size()) {
+                    n1 = n2;
+                    n2 = n3;
+                    n3 = n1 + n2;
+                    s3 = to_string(n3);
+                    tmp += s3;
+                }
+                if (tmp == num) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
