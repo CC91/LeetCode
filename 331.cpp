e5ba62214021267abed14662283a45d9168cba47
CC <caocancabbage@gmail.com>
2016-06-07 10:56:31 -0500

    331

--- 331.cpp
+++ 331.cpp
@@ -25,3 +25,36 @@ public:
         return count == 0;
     }
 };
+
+
+
+class Solution {
+public:
+    bool isValidSerialization(string preorder) {
+        if (preorder.empty()) {
+            return false;
+        }
+        stack<string> s;
+        istringstream iss(preorder);
+        vector<string> tmp;
+        string str;
+        while (getline(iss, str, ',')) {
+            tmp.push_back(str);
+        }
+        for (string t : tmp) {
+            if (t == "#") {
+                while (!s.empty() && s.top() == "#") {
+                    s.pop();
+                    if (s.empty() || s.top() == "#") {
+                        return false;
+                    }
+                    s.pop();
+                }
+                s.push("#");
+            } else {
+                s.push(t);
+            }
+        }
+        return s.size() == 1 && s.top() == "#";
+    }
+};
