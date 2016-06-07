2c89f1f86faee3d1e43e453de646b8a81d1a81ce
CC <caocancabbage@gmail.com>
2016-06-07 10:56:42 -0500

    343

--- 343.cpp
+++ 343.cpp
@@ -18,7 +18,9 @@ class Solution {
 public: // DP
     int integerBreak(int n) {
         vector<int> dp{0, 0, 1, 2, 4, 6, 9};
-        for (int i=7; i<=n; i++) dp.push_back(dp[i-3]*3);
+        for (int i=7; i<=n; i++) {
+            dp.push_back(dp[i-3]*3);
+        }
         return dp[n]; // dp.back() is wrong since n maybe < 7
     }
 };
