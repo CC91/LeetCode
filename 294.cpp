b65038986a710b71366ab56bd4c52ad7509c79d3
CC <caocancabbage@gmail.com>
2016-06-09 18:10:39 -0500

    294

---
+++ 294.cpp
@@ -0,0 +1,13 @@
+// 294. Flip Game II
+
+class Solution {
+public:
+    bool canWin(string s) {
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i - 1] == '+' && s[i] == '+' && !canWin(s.substr(0, i - 1) + "--" + s.substr(i + 1))) {
+                return true;
+            }
+        }
+        return false;
+    }
+};
